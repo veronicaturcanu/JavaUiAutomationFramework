e5de3df344625a5df0942bd7c8a3601cea0314f0
veronicaturcanu <veronica.turcanu.adam@gmail.com>
2023-10-16 18:31:07 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,31 @@
 package com.opencart;
 
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.ScriptTimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        String currentWindowName = driver.getWindowHandle();
+
+        //new Window Code
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.google.com/");
+        Thread.sleep(1000);
+        System.out.println(driver.getTitle());
+        driver.close(); //permite inchiderea unui Tab
+
+        driver.switchTo().window(currentWindowName);
+        driver.get("https://tekwill.md/");
+        Thread.sleep(1000);
+        System.out.println(driver.getTitle());
+        driver.quit(); //permite inchiderea instantei (driver)
+
+        System.out.println("The execution is over.");
     }
 }
